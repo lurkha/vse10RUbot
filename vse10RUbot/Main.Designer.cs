759f646add76ad230493b098726090b132aa85a1
lurkha <lur0kha@gmail.com>
2019-07-09 23:35:02 +0300

    Add project files.

---
+++ vse10RUbot/Main.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace vse10RUbot
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "lur0kha@gmail.com";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(220, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "dniwe1337";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(15, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(293, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Русский сертификат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(15, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(293, 26);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Английский сертификат";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // log
+            // 
+            this.log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.log.Location = new System.Drawing.Point(314, 9);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(280, 158);
+            this.log.TabIndex = 6;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(15, 61);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(293, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 10;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 179);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(622, 171);
+            this.Name = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ vse10RUbot/Main.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Remote;
+using System.Windows.Input;
+using AutoIt;
+
+namespace vse10RUbot
+{
+    public partial class Main : Form
+    {
+        ChromeDriver CD;
+        
+       string Login
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+        }
+        string Password
+        {
+            get
+            {
+                return textBox2.Text;
+            }
+        }
+
+        public Main()
+        {
+            ChromeOptions OP = new ChromeOptions();
+            OP.AddArgument("--allow-no-sandbox-job");
+            ChromeDriverService SERVICE = ChromeDriverService.CreateDefaultService();
+            //SERVICE.HideCommandPromptWindow = true;
+            CD = new ChromeDriver(SERVICE, OP);
+             InitializeComponent();
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Auth(textBox1.Text, textBox2.Text);
+            CommitTest(TestLang.RUS);
+        }
+
+        public void Auth(string login, string password)
+        {
+            CD.Navigate().GoToUrl("https://www.ratatype.ru/exit/");
+            CD.Navigate().GoToUrl("https://www.ratatype.ru/login/");
+            ///////////////
+            CD.FindElementByXPath("//input[@name='login']").Clear();
+            CD.FindElementByXPath("//input[@name='login']").SendKeys(Login);
+                CD.FindElementByXPath("//input[@name='password']").SendKeys(Password);
+                ///////////////
+                CD.FindElementByXPath("//button[@type='submit']").Submit();
+                ///////////////
+                log.Text += "Логин успешен! \n";
+                //////////////
+        }
+        public bool CommitTest(TestLang lang)
+        {
+            if (lang == TestLang.RUS)
+            {
+               
+                CD.Navigate().GoToUrl($"https://www.ratatype.ru/typing-test/test/"); 
+                
+                
+            }
+            else
+            {
+                CD.Navigate().GoToUrl($"https://www.ratatype.ru/typing-test/test/en");
+            }
+
+            CD.FindElementByXPath("//button[@class='submit gr']").Click();
+            var Elements = CD.FindElementsByXPath("//div[@class='mainTxt']/span");
+            var TextField = CD.FindElementByXPath("//div[@class='divTextarea']/textarea");
+            InputLanguage.CurrentInputLanguage = InputLanguage.FromCulture(new System.Globalization.CultureInfo("ru-RU"));
+            AutoItX.MouseClick("LEFT", TextField.Location.X, TextField.Location.Y, 1);
+
+
+            foreach (var item in Elements)
+            {
+                Thread.Sleep((this.trackBar1.Value));
+                string TextToSend = item.Text;
+                if (TextToSend == "")
+                {
+                    AutoItX.Send(" ");
+                }
+                else
+                {
+                    AutoItX.Send(TextToSend);
+                }
+
+                //TextField.SendKeys(TextToSend);
+
+
+            }
+            return true;
+        }
+        public enum TestLang
+        {
+            RUS = 1,
+            ENG = 2
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Auth(textBox1.Text, textBox2.Text);
+            CommitTest(TestLang.ENG);
+        }
+    }
+}
